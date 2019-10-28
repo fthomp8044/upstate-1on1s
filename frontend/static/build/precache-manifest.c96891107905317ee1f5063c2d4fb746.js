f35f25ef568723be9fb00208df8ff56c66155534
fthomp8044 <fthomp2430@gmail.com>
2019-10-28 16:27:46 -0400

    update singup

---
+++ frontend/static/build/precache-manifest.c96891107905317ee1f5063c2d4fb746.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ece4fd722e952f5cfe4841892f8ef4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49fe5e6160a4205295a7",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "ac14dcb2f6342eaf3168",
+    "url": "/static/css/main.e01c8df7.chunk.css"
+  },
+  {
+    "revision": "49fe5e6160a4205295a7",
+    "url": "/static/js/2.6fde0370.chunk.js"
+  },
+  {
+    "revision": "ac14dcb2f6342eaf3168",
+    "url": "/static/js/main.4266b1c4.chunk.js"
+  },
+  {
+    "revision": "71dcedd2f2bbe7dcbd91",
+    "url": "/static/js/runtime-main.107b9db7.js"
+  }
+]);
