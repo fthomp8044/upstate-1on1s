57818a0431a107c2d8e2e99628bffdeb01f142e1
fthomp8044 <fthomp2430@gmail.com>
2019-10-29 10:31:53 -0400

    update index component

--- frontend/static/src/components/Index.js
+++ frontend/static/src/components/Index.js
@@ -12,8 +12,8 @@ render() {
         <h1 className='display-3'>1on1s</h1>
         <p className='lead'>Train with The Upstates finest!</p>
         <hr/>
-        <a class='btn btn-light btn-lg' href='/SignUp' role='button'>Sign Up</a>
-        <a class='btn btn-dark btn-lg' href='/Login' role='button'>Login</a>
+        <a class='btn btn-light btn-lg' href='/signup' role='button'>Sign Up</a>
+        <a class='btn btn-dark btn-lg' href='/login' role='button'>Login</a>
       </div>
     </div>
   )
